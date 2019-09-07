09f64c9787c622fe22561e944bbbb17011f19dec
adeministratorTr <adembavas@gmail.com>
2019-09-07 17:58:38 +0300

    Add post actions

---
+++ src/actions/post.js
@@ -0,0 +1,44 @@
+import api from 'helpers/api';
+
+export const types = {
+  POSTS_START: 'POSTS/FETCH_POSTS_START',
+  POSTS_SUCCESS: 'POSTS/FETCH_POSTS_SUCCESS',
+  POSTS_ERROR: 'POSTS/FETCH_POSTS_ERROR',
+  GET_POST_START: 'POST/FETCH_SELECTED_POST_START',
+  GET_POST_SUCCESS: 'POST/FETCH_SELECTED_POST_SUCCESS',
+  GET_POST_ERROR: 'POST/FETCH_SELECTED_POST_ERROR'
+}
+
+export const fetchAllPosts = () => dispatch => {
+  dispatch({ type: types.POSTS_START });
+  return api.getAllPosts()
+    .then((result) => {
+      dispatch({
+        type: types.POSTS_SUCCESS,
+        posts: result
+      })
+    })
+    .catch((e)=> {
+      dispatch({
+        type: types.POSTS_ERROR
+      }, () => { console.log('API Posts Error: ', e) })
+    });
+}
+
+export const fetchPostDetail = (postId) => dispatch => {
+  dispatch({
+    type: types.GET_POST_START
+  });
+  return api.getPostDetail(postId)
+    .then((result) => {
+      dispatch({
+        type: types.GET_POST_SUCCESS,
+        post: result
+      })
+    })
+    .catch((e) => {
+      dispatch({
+        type: types.GET_POST_ERROR
+      }, () => { console.log('API Post Error: ', e) })
+    })
+}
