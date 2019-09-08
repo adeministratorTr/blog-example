e8530c90bf4ad58ce57e83d9c757fbdf679ba343
adeministratorTr <adembavas@gmail.com>
2019-09-08 10:13:17 +0300

    Add Search to PostList page

--- src/containers/PostList/index.js
+++ src/containers/PostList/index.js
@@ -1,36 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import memoize from 'memoize-one';
 
 import { fetchAllPosts } from 'actions/post';
 
 import PostItem from 'components/PostItem';
 
 class PostList extends React.Component {
+  state = {
+    filterText: ''
+  }
+
+  filter = memoize(
+    (postList, filterText) => postList.filter(item => item.title.includes(filterText))
+  );
+  
   componentDidMount() {
     this.props.fetchAllPosts();
   }
-  
+
+  handleSearchOnChange = (event) => {
+    this.setState({
+      filterText: event.target.value.toLowerCase()
+    })
+  }
+
   render() {
+    const { filterText } = this.state;
     const { postList, isLoading } = this.props;
+    const filteredList = this.filter(postList, filterText);
+
     return(
       <React.Fragment>
         {isLoading && <p>Loading...</p>}
+        <input type="text" value={filterText} onChange={this.handleSearchOnChange} />
 
-        {!isLoading && postList && postList.length > 0 && 
-          postList.map(post => 
-            <PostItem 
-              key={post.id}
-              title={post.title}
-              body={post.body}
-            />
-          )
+        {!isLoading && filteredList && filteredList.length > 0 && 
+          <React.Fragment>
+            {filteredList.map(post => 
+              <PostItem 
+                key={post.id} 
+                title={post.title}
+                body={post.body}
+              />
+            )}
+          </React.Fragment>
         }
+        {!isLoading && filteredList.length === 0 && <p>No result</p> }
       </React.Fragment>
     )
   }
 }
-
-
 
 export default connect(
   (state) => ({
