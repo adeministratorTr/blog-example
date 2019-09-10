9f67eb56865f96fd063ae674e60b6ea7f9f1b74e
adeministratorTr <adembavas@gmail.com>
2019-09-10 20:33:36 +0300

    Add read more button and update styles

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import Button from 'components/Button';
+
 import './style.css';
 
 const PostItem = (props) =>
-  <div className='post-item'>
-    <h1>{props.title}</h1>
-    <p>{props.body}</p>
+  <div className="post-item">
+    <h2>{props.title}</h2>
+    <div className="read-more">
+      <p>{props.body}</p>
+      <Button 
+        type="redirect"
+        buttonText="Read More.."
+        onClick={props.handleReadMoreClick}
+        />
+    </div>
   </div>
 
 export default PostItem;
